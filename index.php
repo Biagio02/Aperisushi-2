4ca25a6273925a9d24a8cabd4a9389a7a00dc5a0
Biagio02 <106422756+Biagio02@users.noreply.github.com>
2022-05-28 08:39:03 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Fig4hperiSushi</title>
+  </head>
+
+  <body>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            <img src="./images/LG.png" alt="" width="150" height="120">
+            Fig4hperiSushi
+          </a>  
+        </div>
+    </nav>
+    
+    <div class="container my-5 text-center" style="max-width: 580px;">
+
+        <h1>Se non fai l'AperiSushi Fig4h sei un bauscia</h1>
+        <p class="lead">Perché fig4h se il sushi costa è buono, non ce n'è. 
+            Poi se a te piace il Sushi che non è buono fig4h fai pure, io ti consigio di provare il nostro.
+            Fig4h Sushi per Aperitivo fig4h scherzi?!
+        </p>
+    </div>
+
+    <div class="container my-5 text-center" style="max-width: 800px;">
+        <div class="row">
+            <div class="col-sm">
+                <form action="prenotazioni.php" method="POST">
+                    <button type="submit" class="btn btn-outline-dark">Fig4h prenoti?</button>
+                </form>
+            </div>
+            <br>
+            <br>
+            <br>
+            <div class="col-sm">
+                <form action="impTavolo.php">
+                    <button type="submit" class="btn btn-outline-dark">Fig4h ti siedi?</button>
+                </form>
+            </div>
+            <br>
+            <br>
+            <br>
+            <div class="col-sm">
+                <button type="submit" class="btn btn-outline-dark">Fig4h d'asporto?</button>
+            </div>
+        </div>
+    </div>
+    
+   
+            
+        </div>
+    </div>
+      </div><!--contenitore -->
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>  
+     </body>
+</html>
